f2bfb0b1e238fb324abc717795271ddb1b0fca87
FlyingGasmask <56984276+FlyingGasmask@users.noreply.github.com>
2019-10-25 02:29:21 +0200

    Add files via upload

---
+++ F-16 Swedish AF/description.lua
@@ -0,0 +1,25 @@
+livery = {
+--Livery By FlyingGasmask 
+
+--Diffuse  -----------------------------------
+
+{"F16_bl50_Kil", 0 ,"F16_bl50_Kil",false};
+{"F16_bl50_Kil", ROUGHNESS_METALLIC ,"F16_bl50_Kil_RoughMet",false};
+{"F16_bl50_Main_1", 0 ,"F16_bl50_Main_1",false};
+{"F16_bl50_Main_1", ROUGHNESS_METALLIC ,"F16_bl50_Main_1_RoughMet",false};
+{"F16_bl50_Main_2", 0 ,"F16_bl50_Main_2",false};
+{"F16_bl50_Main_3", 0 ,"F16_bl50_Main_3",false};
+{"F16_bl50_wing_L", 0 ,"F16_bl50_wing_L",false};
+{"F16_bl50_wing_L", ROUGHNESS_METALLIC ,"F16_bl50_wing_L_RoughMet",false};
+{"F16_bl50_Wing_Pylon_1", 0 ,"F16_bl50_Wing_Pylon_1",false};
+{"F16_bl50_Wing_Pylon_2", 0 ,"F16_bl50_Wing_Pylon_2",false};
+{"F16_bl50_wing_R", 0 ,"F16_bl50_wing_R",false};
+{"F16_bl50_wing_R", ROUGHNESS_METALLIC ,"F16_bl50_wing_R_RoughMet",false};
+{"pilot_F16_helmet", 0 ,"pilot_F16_helmet",false};
+{"pilot_F16", 0 ,"pilot_F16",false};
+{"F16_bl50_Glass", 0 ,"F16_bl50_Glass",false};
+{"pilot_F16_patch", 0 ,"pilot_F16_patch",false};
+}
+name = "Swedish Air Force Gripen"
+
+--countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
